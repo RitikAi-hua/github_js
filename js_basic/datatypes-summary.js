d50bee31e593cecdf2ae6a5cf56e5e9e22ca38c5
RitikAi-hua <ritikjaiswalkalwar@gmail.com>
2026-05-31 21:10:39 +0000

    stack and heap

--- js_basic/datatypes-summary.js
+++ js_basic/datatypes-summary.js
@@ -7,7 +7,7 @@
 //const score=?(any) const score:number=20type safety
 const Id=Symbol('123')// they looked same but are different due to symbol
 const anotherId=Symbol('123')
-console.log(Id===anotherId)
+//console.log(Id===anotherId)
 
 const bigNumber=345654324574533n
 //***non primitive****
@@ -22,6 +22,27 @@ const myFunction=function(){
     console.log("Hello World");
 }
 
-//console.log(typeof myFunction);
+// console.log(typeof myFunction);
 
-myObj.name
+// console.log(myObj.name)
+ //++++++++memory types in javascript++++++
+ //Stack(all primitive )store inside stack,Heap(all Non primitive) store inside heap 
+//  for stack memory we get copy of it
+//  but for  heap we get reference value (org value) any changes reflect to original values
+let myname="ritikjaiswal"
+let anothername=myname
+anothername="Ritesh"
+// console.log(myname)
+
+// console.log(anothername)
+
+let userOne={
+    name:"ritik",
+    age:21
+}
+let userTwo=userOne //userone and usertwo both reference on same values so if usertwo make some changes it will shows to userone
+
+userOne.name="Ritesh"
+
+console.log(userTwo.name)
+console.log(userOnename)
