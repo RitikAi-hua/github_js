ce9b0bd6f0965b7c21217c526aeed6eca0d046a9
RitikAi-hua <ritikjaiswalkalwar@gmail.com>
2026-05-31 05:09:06 +0000

    Conversion_of_datatypes

---
+++ js_basic/03_conversionOperation.js
@@ -0,0 +1,21 @@
+let score =true
+
+// console.log(typeof score)
+// console.log(typeof(score))
+// // conversion of string in to number
+// let isLoggedIn=1
+// let valueInNumber=Number(score)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber)
+// con=Boolean(isLoggedIn)
+// console.log(con)
+let someNum=33
+let StringNumber=String(someNum)
+console.log(StringNumber)
+console.log(typeof(StringNumber))
+// "33"=>33
+    // "33abc"=>NaN 
+        // null=>0
+            // undefined=>NaN
+            // true=>1 conversion empty string with Boolean give false
+// conversion always start with capital letter like String Number Boolean
