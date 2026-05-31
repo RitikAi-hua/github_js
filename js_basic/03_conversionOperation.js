7ff66a116b1a32c41a330017ddf8997fee9e8c7e
RitikAi-hua <ritikjaiswalkalwar@gmail.com>
2026-05-31 20:25:59 +0000

    datatypes-summary

--- js_basic/03_conversionOperation.js
+++ js_basic/03_conversionOperation.js
@@ -9,13 +9,43 @@ let score =true
 // console.log(valueInNumber)
 // con=Boolean(isLoggedIn)
 // console.log(con)
-let someNum=33
-let StringNumber=String(someNum)
-console.log(StringNumber)
-console.log(typeof(StringNumber))
+// let someNum=33
+// let StringNumber=String(someNum)
+// console.log(StringNumber)
+// console.log(typeof(StringNumber))
 // "33"=>33
     // "33abc"=>NaN 
         // null=>0
             // undefined=>NaN
             // true=>1 conversion empty string with Boolean give false
 // conversion always start with capital letter like String Number Boolean
+
+// ***********************Operations****************
+//  console.log(2-3)
+//  console.log(2+3)
+//  console.log(2*3)
+//  console.log(2/3)
+//  console.log(2%3)
+//  console.log(2**3)
+
+let str1="hello"
+let str2=" ritik"
+let str3=str1 + str2
+console.log(str3);
+console.log("2" + 3);
+console.log("1" + 2 + 2); //122 rule-->
+console.log(1 + 2 + "2"); //32
+// console.log(true) true
+
+// console.log(null >0)//false
+// console.log(null ==0)  false     // these all 6 create conusion
+// console.log(null >=0)true    comparision operators like <><=>= all convert null to a number(0) but equality not convert it
+// console.log(undefined==0)  for undefined gives false for all
+// console.log(undefined=>)
+// console.log(undefined<0)
+
+console.log("2">1)
+
+// lossy check(==) and strict check(===)
+// for loosy check before comparision it type converse value 
+// but for strict check it doesn't type cast datatypes before camparision
