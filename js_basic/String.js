e071f388882d2fbad22fb12a5fadeaa9cb25ab62
RitikAi-hua <ritikjaiswalkalwar@gmail.com>
2026-06-02 05:30:10 +0000

    String_fxn

--- js_basic/String.js
+++ js_basic/String.js
@@ -1,7 +1,15 @@
 // String interpolation ${value you want}
 const name=" Ritik"
 const repoCount=50
-console.log(`Hello my name is ${name.strip()} and my repo count is ${repoCount}`)
+console.log(`Hello my name is ${name.strip} and my repo count is ${repoCount}`)
 //Another way to declare String
 const gameName=new String('Ritik jaiswal')
 console.log(gameName.__proto__) //{}
+console.log(name.charAt(2))
+// .replace(Find ,replace)
+// .trim()
+// .includes(from string )
+//  #check words present or not
+//.join(joiner)
+const str="Rit-ik-jai";
+console.log(str.split('-'))
