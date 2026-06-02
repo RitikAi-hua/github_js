2f9436c496381baf3c8d3c6e8d64dc704fcd052c
RitikAi-hua <ritikjaiswalkalwar@gmail.com>
2026-06-02 04:51:36 +0000

    String

---
+++ js_basic/String.js
@@ -0,0 +1,7 @@
+// String interpolation ${value you want}
+const name=" Ritik"
+const repoCount=50
+console.log(`Hello my name is ${name.strip()} and my repo count is ${repoCount}`)
+//Another way to declare String
+const gameName=new String('Ritik jaiswal')
+console.log(gameName.__proto__) //{}
